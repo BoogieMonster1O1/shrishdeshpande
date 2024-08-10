5a0c653925b1b523f23ccce25b475ab495e4654f
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-08-10 22:25:53 +0530

    pls work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 
@@ -9,5 +8,5 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shrishdeshpande.com',
-  integrations: [tailwind(), icon(), sitemap(), svelte(), mdx()]
+  integrations: [tailwind(), sitemap(), svelte(), mdx()]
 });
